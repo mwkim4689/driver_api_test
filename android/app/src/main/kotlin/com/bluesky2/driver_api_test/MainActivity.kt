9b4f1475db1ff2af66b0b989b3712f01dd84efc8
striver89 <striver89@gmail.com>
2021-03-28 17:33:51 +0900

    first comit for api test

---
+++ android/app/src/main/kotlin/com/bluesky2/driver_api_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bluesky2.driver_api_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Driver API Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Driver API Test'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  String base_url = 'https://api.godokdriver.com';
+  String responseData = '';
+
+  void _login() async {
+    FormData formData = FormData.fromMap({
+      'auth': {
+        'email': 'test@test.com',
+        'password': 'test1234',
+      }
+    });
+
+    final response = await getDio().post(
+      "/api/v1/auth/login",
+      data: formData,
+      // {
+      //   "auth": {"email": "test@test.com", "password": "test1234"}
+      // },
+    );
+
+    print('response.data : ${response.data}');
+    setState(() {
+      responseData = response.data;
+    });
+  }
+
+  Dio getDio({
+    String token,
+  }) {
+    // Map<String, dynamic> headers = Map();
+    // if (!isNullOrBlank(token)) {
+    // headers['Authorization'] = 'Bearer $token';
+    // headers['Content-Type'] = 'application/json';
+    // headers['Content-Type'] = 'multipart/form-data';
+    // }
+
+    BaseOptions options = BaseOptions(
+        baseUrl: base_url,
+        // headers: {'Authorization': 'Bearer $token'},
+        contentType: Headers.jsonContentType);
+
+    Dio dio = Dio(options);
+    dio.interceptors.add(LogInterceptor());
+    dio.interceptors.add(LogInterceptor(requestBody: true));
+    return dio;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              responseData,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _login,
+        tooltip: 'Login Test Button',
+        child: Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
